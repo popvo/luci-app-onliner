6e38b350ce76b34a005e6e626413f4ccd80ae7b6
popvo <soft10@163.com>
2021-04-30 14:04:45 +0800

    Update onliner.lua

--- luasrc/controller/onliner.lua
+++ luasrc/controller/onliner.lua
@@ -1,7 +1,7 @@
 module("luci.controller.onliner",package.seeall)
 nixio=require"nixio"
 function index()
-entry({"admin","status","onliner"},alias("admin","status","onliner","onliner"),_("Online User"))
+entry({"admin","status","onliner"},alias("admin","status","onliner","onliner"),_("联机用户"))
 entry({"admin","status","onliner","onliner"},template("onliner/onliner"),_("Online User"),1)
 entry({"admin", "status","onliner","speed"}, template("onliner/display"), _("speed"), 2)
 entry({"admin", "status","onliner","setnlbw"}, call("set_nlbw"))
